c123f7c8044c189f7507c15756aa409cea2f33d5
Hari Pokala <hari.kpokala@gmail.com>
2026-04-19 15:51:40 -0400

    Update Terraform workflow to simplify plan command and add new variables for AWS region, environment, and project name

--- variables.tf
+++ variables.tf
@@ -81,3 +81,15 @@ variable "platform_admin_principal_arns" {
   type        = list(string)
   default     = []
 }
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "project_name" {
+  default = "eks-starter"
+}
